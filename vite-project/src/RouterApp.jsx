178a48191ce9dfc0c8d0440567d5876f24dd0c65
Madhav P <madhavp2023@gmail.com>
2025-06-30 07:30:33 +0530

    Learning Phase-8 Completed

--- vite-project/src/RouterApp.jsx
+++ vite-project/src/RouterApp.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Hello from "./components/Hello.jsx";
 import Bye from "./components/Bye.jsx";
 import Navbar2 from "./components2-Sigma/RouterNavbar2.jsx";
-import User from "./components2-Sigma/User.jsx";
+import User from "./components2-Sigma/RouterUser.jsx";
 
 const router = createBrowserRouter([
   {
